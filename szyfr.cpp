3762867163d60f1fbe48fb42fb2ce74bc4f854ed
Jakub <mra120603@gmail.com>
2022-12-18 16:36:21 +0100

    cipher of delta is working corretlty and waiting to implement

---
+++ szyfr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int b,c;
+int a = 11;
+string tekst;
+int delta;
+int ret_data;
+
+
+void szyfr(int a, int b, int c);
+void deszyfr(int a, int b, int c);
+
+int main()
+{
+    getline(cin,tekst);
+    c = tekst.length();
+    cout << c << endl;
+    szyfr(a,b,c);
+}
+void szyfr(int a, int b, int c)
+{
+    for (int i=0;i<tekst.length();i++)
+    { 
+        b = int(tekst[i]);
+        delta = pow(b,2) - (4*a*c);
+        ret_data = sqrt(delta+4 * a *c);
+        cout << delta << " " << ret_data << " " << char(b) << endl;
+    }
+}
+
+    
