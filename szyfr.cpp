90a1a49919f0a4dace37b6fd2861d1776874b423
Jakub <mra120603@gmail.com>
2022-12-21 14:02:28 +0100

    small update to alorytm

--- szyfr.cpp
+++ szyfr.cpp
@@ -9,7 +9,6 @@ string tekst;
 int delta;
 int ret_data;
 
-
 void szyfr(int a, int b, int c);
 void deszyfr(int a, int b, int c);
 
@@ -23,12 +22,10 @@ int main()
 void szyfr(int a, int b, int c)
 {
     for (int i=0;i<tekst.length();i++)
-    { 
+    {
         b = int(tekst[i]);
         delta = pow(b,2) - (4*a*c);
         ret_data = sqrt(delta+4 * a *c);
-        cout << delta << " " << ret_data << " " << char(b) << endl;
+        cout << delta << endl;
     }
 }
-
-    
